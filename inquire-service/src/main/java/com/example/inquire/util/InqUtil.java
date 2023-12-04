99e7fa33f50e8930767e5e2c7adef764c10bc2b7
Devloper <devloper@git.org>
2023-12-04 23:51:49 +0530

    sonarlint issue resolve

--- inquire-service/src/main/java/com/example/inquire/util/InqUtil.java
+++ inquire-service/src/main/java/com/example/inquire/util/InqUtil.java
@@ -3,11 +3,9 @@ package com.example.inquire.util;
 import com.example.inquire.model.Product;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -15,8 +13,13 @@ import java.util.List;
 public class InqUtil {
 
     public static final String DOWNSTREAM_INTERFACE_CALL = "productservice";
-    @Autowired
-    RestTemplate restTemplate;
+
+    private RestTemplate restTemplate;
+
+    public InqUtil(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @CircuitBreaker(name = DOWNSTREAM_INTERFACE_CALL, fallbackMethod = "callFallBack")
     public List<Product> productByProductService() {
         return restTemplate.getForObject("http://localhost:8086/products", List.class);
